6d0a39a97e2fd51f6403b67f5a2e5fb1f0e2a1d0
Sylvie <sylvie.veyres@gmail.com>
2017-08-02 14:58:41 +0200

    admin : add/edit/remove testimonials

--- svtestimonials.php
+++ svtestimonials.php
@@ -4,3 +4,4 @@ if (!defined('_PS_VERSION_')) {
 }
 
 require_once _PS_MODULE_DIR_.'svtestimonials/classes/SvTestimonials.php';
+require_once _PS_MODULE_DIR_.'svtestimonials/models/TestimonialsPost.php';
